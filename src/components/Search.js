43b150a12b55f0cb6f885184adf67c239011231e
Vern Saeteurn <vernchiem2000@gmail.com>
2022-01-23 01:35:46 -0800

    168. Searching on initial render

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,8 +20,41 @@ const Search = () => {
       setResults(data.query.search)
     }
 
-    search()
+    if (term && !results.length) {
+      search()
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) search()
+      }, 500)
+
+      // cleanup 
+      return () => {
+        clearTimeout(timeoutId)
+      }
+    }
+
   }, [term])
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
 
   return (
     <div>
@@ -34,6 +67,9 @@ const Search = () => {
             className="input"
           />
         </div>
+      </div>
+      <div className="ui celled list">
+        {renderedResults}
       </div>
     </div>
   )
